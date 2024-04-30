450935cfefea30296afed0ec45c0a4dc3168bbf4
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2024-04-30 11:55:53 -0400

    Switching to official OpenAI node package, since it now supports edge functions.

--- pages/api/openaiimage.tsx
+++ pages/api/openaiimage.tsx
@@ -1,16 +1,13 @@
 // Next.js
 import type { NextRequest } from "next/server"
 
-// OpenAI package, but the edge version
-import { Configuration, OpenAIApi, ResponseTypes } from "openai-edge"
+// OpenAI package
+import OpenAI from 'openai';
 
 // Set the API key
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-// Init the main module
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
 const handler = async (req: NextRequest) => {
 
@@ -19,17 +16,15 @@ const handler = async (req: NextRequest) => {
     const body = await req.json();
 
     // Send the request to the robots
-    const image_generated = await openai.createImage({
+    const image_generated = await openai.images.generate({
+      model: "dall-e-3",
       prompt: body.item_name,
       n: 1,
       size: '1024x1024',
     });
 
-    // Wait for the robots to respond
-    const data = (await image_generated.json()) as ResponseTypes["createCompletion"]
-
     // Yay! They responded! Let's tell the frontend!
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify(image_generated.data), {
       status: 200,
       headers: {
         "content-type": "application/json",
--- pages/api/openainame.tsx
+++ pages/api/openainame.tsx
@@ -1,34 +1,26 @@
 // Next.js
 import type { NextRequest } from "next/server"
 
-// OpenAI package, but the edge version
-import { Configuration, OpenAIApi, ResponseTypes } from "openai-edge"
+// OpenAI package
+import OpenAI from 'openai';
 
 // Set the API key
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-// Init the main component
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
 const handler = async (req: NextRequest) => {
 
   try {
 
     // Try getting a name
-    const item_name = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Generate the name of an absurd imaginary product.",
-      max_tokens: 50,
-      stream: false,
+    const item_name = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Generate the name of an absurd imaginary product.' }],
+      model: 'gpt-3.5-turbo',
     });
 
-    // Await the response, so we can do something with it
-    const data = (await item_name.json()) as ResponseTypes["createCompletion"]
-
     // Yay! We did it! Return that response!
-    return new Response(JSON.stringify(data.choices[0].text.trim().replace("\n", "").replace("\"", "").replace("\\\"", "")), {
+    return new Response(JSON.stringify(item_name.choices[0].message.content.trim().replace("\n", "").replace("\"", "").replace("\\\"", "")), {
       status: 200,
       headers: {
         "content-type": "application/json",
